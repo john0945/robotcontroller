2c11c17df2abc0bd0ceda0006c46a26f57dc724d
john0945 <johnlewis0945@gmail.com>
2016-08-31 15:29:22 +0200

    asdf

--- direction.js
+++ direction.js
@@ -13,11 +13,15 @@ var main = function() {
 			was_driving = true;
 			stop_driving();
 		}
-		
+		var command = {}; // create an empty object
+
 		switch(current_id){
 			
 			case 'left':
-			alert("left");
+				command.action = "left";
+				command.amount = "1";
+				send(JSON.stringify(command));
+
 			break;
 			case 'drive':
 				//if drive was pressed, and we weren't just driving, then start driving
@@ -29,10 +33,11 @@ var main = function() {
 				
 			break;
 			case 'right':
-			alert("right");
+				command.action = "right";
+				command.amount = "1";
+				send(JSON.stringify(command));
 			break;
 			case 'shoot':
-			alert("shoot");
 			break;
 			case 'avoid':
 			alert("avoid");
@@ -46,14 +51,86 @@ var main = function() {
 }
 
 var stop_driving  = function(){
-	
-	alert("stop driving");
+
+	var command = {};
+	command.action = "stop";
+	command.amount = "0";
+	send(JSON.stringify(command));
 }
 var start_driving= function(){
-	
-	alert("start driving");
+	var command = {}; // create an empty object
+	command.action = "forward"; // create a name property based on the input field's value
+	command.amount = "0";
+	send(JSON.stringify(command));
+}
+
+
+function send(command) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "https://httpbin.org/post", true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.onreadystatechange = function(){
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			console.log(xhr.responseText);
+		}
+	};
+	xhr.send(hero);
 }
 
 
 
+function request(sensor) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			document.getElementById("output").innerHTML = xhr.responseText;
+		}
+	}
+	xhr.open("GET", url, true);
+	xhr.send();
+	document.getElementById("output").innerHTML = "Waiting for response ...";
+}
+
+
+function register(){
+	var appId = 'Prime';
+	var url = 'http://137.158.126.10:4000/m2m/applications';
+	var data = {'application': {'appId': appId}};
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    console.log(data);
+	xhr.onreadystatechange = function(){
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			console.log(xhr.responseText);
+		}
+	};
+	xhr.send(data);
+    //container();
+}
+
+function container()
+{
+
+    var appId = 'Prime';
+    var url = 'http://137.158.126.10:4000/m2m/applications/Prime/containers';
+    var data = {'container': {'id': 'Prime'}}
+
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    //xhr.setRequestHeader("Content-Type", "application/json");
+    console.log(data);
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.send(data);
+
+}
+
+
+$(document).ready(register);
 $(document).ready(main);
---
+++ scripts.js
@@ -0,0 +1,48 @@
+/**
+ * Created by johnl on 2016/08/31.
+ */
+var text = document.getElementById("text");
+var html = document.getElementById("html");
+var api = document.getElementById("api");
+var myip = 0 ;
+
+text.addEventListener("click", function(){ request("https://s3.amazonaws.com/sitepoint-book-content/jsninja/hello.txt") }, false);
+html.addEventListener("click", function(){ request("https://s3.amazonaws.com/sitepoint-book-content/jsninja/hello.htm") }, false);
+api.addEventListener("click", function(){ request("http://ip.jsontest.com/") }, false);
+// var form = document.forms.hero;
+// form.addEventListener("submit", makeHero, false);
+function request(url) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            myip = JSON.parse(xhr.response);
+            alert(myip.ip);
+            document.getElementById("output").innerHTML = xhr.responseText;
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
+    document.getElementById("output").innerHTML = "Waiting for response ...";
+}
+
+
+function makeHero(event) {
+    event.preventDefault(); // prevent the form from being submitted
+    var hero = {}; // create an empty object
+    hero.name = form.name.value; // create a name property based on the input field's value
+    send(JSON.stringify(hero));
+}
+
+function send(hero) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://httpbin.org/post", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4 && xhr.status === 201) {
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.send(hero);
+}
+
